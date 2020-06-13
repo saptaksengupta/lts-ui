3306a7844e87dd84ad954067a4b681127ee8b0d8
saptaksengupta <deeps.sengupta@gmail.com>
2020-06-13 17:32:15 +0530

    Updates

---
+++ precache-manifest.fb4b57059139e5b57eabe23a2d5b7b07.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a8e2f392dcf293ef7d15c0ee449915",
+    "url": "/lts-ui/index.html"
+  },
+  {
+    "revision": "5c22d6632eac1d1fecfe",
+    "url": "/lts-ui/static/css/main.eb246645.chunk.css"
+  },
+  {
+    "revision": "766d716e9a0ea4f3fa9b",
+    "url": "/lts-ui/static/js/2.9f613162.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/lts-ui/static/js/2.9f613162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c22d6632eac1d1fecfe",
+    "url": "/lts-ui/static/js/main.1a836cb6.chunk.js"
+  },
+  {
+    "revision": "faeadd362d8afa178aae",
+    "url": "/lts-ui/static/js/runtime-main.743a6bf3.js"
+  },
+  {
+    "revision": "47ac8c9f3d1d29ac6ef1db28fbd35846",
+    "url": "/lts-ui/static/media/Curved-card.47ac8c9f.svg"
+  },
+  {
+    "revision": "0f70348628d9637b90f7c6f36d8b6e66",
+    "url": "/lts-ui/static/media/Liquid-Cheese.0f703486.svg"
+  },
+  {
+    "revision": "093bfad28c0d039977b24d502cca15cb",
+    "url": "/lts-ui/static/media/undraw_completed.093bfad2.svg"
+  },
+  {
+    "revision": "6380db9c63e56223400c81938e4ac169",
+    "url": "/lts-ui/static/media/undraw_no_data_qbuo.6380db9c.svg"
+  }
+]);
